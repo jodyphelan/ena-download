43fcb3a71f52df0472e5d2b004cb230ba8525b0a
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-02-06 13:03:39 +0100

    bump version

--- ena_download/__init__.py
+++ ena_download/__init__.py
@@ -2,7 +2,7 @@
 This package facilitates the download of data from the ENA in fastq format.
 To use it, you need to provide the accession number of the data you want to download.
 """
-__version__ = '0.1.3'
+__version__ = '0.2.0'
 import requests
 import os
 import subprocess as sp
